175c911d6b4057bb902173c4745c14c20b052c27
Mayur <agnani.mayur@gmail.com>
2023-11-02 00:38:09 -0400

    refactored code

--- app.py
+++ app.py
@@ -9,7 +9,10 @@ import random
 import calendar
 import datetime
 
-app = Flask(__name)
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+app = Flask(__name__)
 
 # Configuration settings
 app.config['MONGO_URI'] = "mongodb+srv://cloudflare:cloudflare@cluster0.d7w8l8l.mongodb.net/?retryWrites=true&w=majority"
@@ -42,7 +45,7 @@ class URLStats:
             if current_time - last_access_time <= 7 * 24 * 60 * 60:
                 url_collection.update_one({"short_url": short_url}, {"$inc": {"7_day_count": 1}})
             url_collection.update_one({"short_url": short_url}, {"$inc": {"all_time_count": 1}})
-            url_collection.update_one({"short_url": short_url}, {"$set": {"last_access_time": current_time})
+            url_collection.update_one({"short_url": short_url}, {"$set": {"last_access_time": current_time}})
         except Exception as e:
             logging.error("An error occurred while updating URL stats: %s", str(e))
 
@@ -57,18 +60,29 @@ def shorten_url():
         return jsonify({"status": False, "error": "Missing Long URL"}), 400
 
     long_url = data["long_url"]
+    date = datetime.datetime.utcnow()
+    utc_time = calendar.timegm(date.utctimetuple())
     try:
         document = url_collection.find_one({"long_url": long_url})
 
         if document:
-            short_url = "http://localhost:5000/tiny/" + document["short_url"]
-            return jsonify({"status": True, "message": "Short URL already exists", "short_url": short_url})
+           
+            expiration_time = data.get("expiration_time", None)
+            if expiration_time and expiration_time < utc_time:
+                return jsonify({"status": True, "message": "Short URL already exists", "short_url": short_url})
+            else:
+                result = url_collection.delete_one({"short_url": short_url})
+                if result.deleted_count == 1:
+                    logging.info("Expired Short URL deleted: %s", short_url)
     except Exception as e:
         logging.error("An error occurred while accessing the database: %s", str(e))
 
     short_url_hash = ShortURL.generate_short_url(long_url)
-    date = datetime.datetime.utcnow()
-    utc_time = calendar.timegm(date.utctimetuple())
+    
+    
+    expiration_time = data.get("expiration_time", None)
+    if expiration_time:
+        expiration_time = int(expiration_time)
 
     message = {
         "creation_time": utc_time,
@@ -78,10 +92,11 @@ def shorten_url():
         "7_day_count": 0,
         "all_time_count": 0,
         "last_access_time": utc_time,
-        "expiration_time": int(data.get("expiration_time", None))
+        "expiration_time": expiration_time
     }
     try:
         url_collection.insert_one(message)
+        logging.info("Short URL created: %s", short_url_hash)
     except Exception as e:
         logging.error("An error occurred while inserting data into the database: %s", str(e))
 
@@ -96,43 +111,44 @@ def get_long_url(short_url):
             date = datetime.datetime.utcnow()
             current_time = calendar.timegm(date.utctimetuple())
             expiration_time = document.get("expiration_time", None)
-            if expiration_time and current_time > expiration_time:
+            if expiration_time and int(current_time) > int(expiration_time):
                 return jsonify({"status": False, "error": "Short URL has expired"}), 400
 
             URLStats.increment_counts(short_url, current_time, url_collection)
             long_url = document["long_url"]
+            logging.info("Short URL accessed: %s", short_url)
             return jsonify({"status": True, "long_url": long_url}), 302
-        else:
-            return jsonify({"status": False, "error": "Short URL not found"}, 404)
     except Exception as e:
         logging.error("An error occurred while accessing the database: %s", str(e))
+    return jsonify({"status": False, "error": "Short URL not found"}, 404)
 
 @app.route("/tiny/<string:short_url>", methods=["DELETE"])
 def delete_short_url(short_url):
     try:
         result = url_collection.delete_one({"short_url": short_url})
         if result.deleted_count == 1:
+            logging.info("Short URL deleted: %s", short_url)
             return jsonify({"status": True, "message": "Short URL deleted"})
-        else:
-            return jsonify({"status": False, "error": "Short URL not found"}, 404)
     except Exception as e:
         logging.error("An error occurred while deleting the short URL: %s", str(e))
+    return jsonify({"status": False, "error": "Short URL not found"}, 404)
 
 @app.route("/tiny/stats/<string:short_url>")
 def get_url_stats(short_url):
     try:
         document = url_collection.find_one({"short_url": short_url})
         if document:
+            logging.info("Stats accessed for: %s", short_url)
             return jsonify({
                 "status": True,
                 "24_hr_count": document["24_hr_count"],
                 "7_day_count": document["7_day_count"],
                 "all_time_count": document["all_time_count"]
             })
-        else:
-            return jsonify({"status": False, "error": "Short URL not found"}, 404)
     except Exception as e:
         logging.error("An error occurred while accessing the database: %s", str(e))
+    return jsonify({"status": False, "error": "Short URL not found"}, 404)
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='tinyurl.log', level=logging.INFO)
     app.run()
